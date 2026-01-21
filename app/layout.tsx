f31ff7588cde8d0e31eee83dafbc6a2b95f5cd44
Andrea Barbieri <barbieriforjob@gmail.com>
2026-01-21 22:49:31 +0100

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Header from "@/components/header_navbar-14";
 import Footer from "@/components/footer_footer-16";
@@ -33,6 +34,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <CookieBanner />
+        <SpeedInsights />
       </body>
     </html>
   );
